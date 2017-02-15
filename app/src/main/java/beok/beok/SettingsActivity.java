d672917d58d9700504a4fbce1344ae21271b3ff9
arthur <arthurtasca@gmail.com>
2017-02-15 13:48:04 -0200

    configurações adicionadas, mas incompletas

---
+++ app/src/main/java/beok/beok/SettingsActivity.java
@@ -0,0 +1,117 @@
+package beok.beok;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by arthur on 14/02/2017.
+ */
+
+public class SettingsActivity extends PreferenceActivity {
+    private static List<String> fragments = new ArrayList<String>();
+
+
+    //@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Add a button to the header list.
+        if (hasHeaders()) {
+            Button button = new Button(this);
+            button.setText("Some action");
+            setListFooter(button);
+        }
+    }
+
+    public void onBuildHeaders(List<Header> target) {
+        loadHeadersFromResource(R.xml.preferences_headers, target);
+        fragments.clear();
+        for (Header header : target) {
+            fragments.add(header.fragment);
+        }
+    }
+
+    @Override
+    protected boolean isValidFragment(String fragmentName) {
+        return fragments.contains(fragmentName);
+    }
+
+
+    public static class PersonalInfoPreference extends PreferenceFragment{
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preference_personal_info);
+        }
+
+    }
+
+    public static class DrogasTratadasPreference extends PreferenceFragment{
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preference_drogas_tratadas);
+        }
+
+    }
+
+    public static class MotivationPreferences extends PreferenceFragment{
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preference_motivation);
+        }
+
+    }
+
+    public static class GoalsPreferences extends PreferenceFragment{
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preference_goals);
+        }
+
+    }
+
+    public static class EmergencyActionsPreferences extends PreferenceFragment{
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preference_emergency_actions);
+        }
+
+    }
+
+    public static class EmergencyContactsPreferences extends PreferenceFragment{
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preference_emergency_contacts);
+        }
+
+    }
+
+}
