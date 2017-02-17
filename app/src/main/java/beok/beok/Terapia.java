f3d2070a8b1c82c7f5e4d6c8c411f011db974d30
William Almeida <william.almeida@usp.br>
2017-02-16 23:03:04 -0200

    Terapia

--- app/src/main/java/beok/beok/Terapia.java
+++ app/src/main/java/beok/beok/Terapia.java
@@ -3,6 +3,8 @@ package beok.beok;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +18,8 @@ public class Terapia extends AppCompatActivity {
 
     LinearLayout layout1, layout2;
     TextView txtpergunta, txtexplicacao;
-    //Button btverdade, btmito, resposta2;
-   // ImageButton btok;
+    Button btverdade, btmito, resposta2;
+    ImageButton btok;
     ImageView imagem;
 
     int pontos = 0;
@@ -28,64 +30,71 @@ public class Terapia extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividade2);
 
-        Resources res = getResources();
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+       /* Resources res = getResources();
         String[] perguntas = res.getStringArray(R.array.pergunta);
 
-        //btmito = (Button) findViewById(R.id.btmito);
-        //btverdade = (Button) findViewById(R.id.btverdade);
+        btmito = (Button) findViewById(R.id.btmito);
+        btverdade = (Button) findViewById(R.id.btverdade);
         txtpergunta = (TextView) findViewById(R.id.txtpergunta);
         layout1 = (LinearLayout) findViewById(R.id.layout1);
         layout2 = (LinearLayout) findViewById(R.id.layout2);
-        //btok = (ImageButton) findViewById(R.id.btok);
+        btok = (ImageButton) findViewById(R.id.btok);
         txtexplicacao = (TextView) findViewById(R.id.txtexplicacao);
         txtpergunta.setText(perguntas[contador]);
         imagem = (ImageView)findViewById(R.id.terapia1);
-        //resposta2 = (Button) findViewById(R.id.resposta1);
+        resposta2 = (Button) findViewById(R.id.resposta1);
     }
 
     public void botaoMito(View v){
-       /* Resources res = getResources();
+        Resources res = getResources();
         String[] respostas = res.getStringArray(R.array.resposta);
         String[] explicacao = res.getStringArray(R.array.explicacao);
         txtexplicacao.setText(explicacao[contador]);
         //Essa parte é so pra mudar a cor do botao que estiver certo (verde = certo, vermelhor = errado)
-       /* if (respostas[contador].equals("mito")){
-            resposta.setBackgroundColor(res.getColor(R.color.verde));
-            resposta.setText("MITO");
+        if (respostas[contador].equals("mito")){
+           // resposta.setBackgroundColor(res.getColor(R.color.verde));
+           // resposta.setText("MITO");
             pontos++;
         }
         else {
-            resposta.setBackgroundColor(res.getColor(R.color.vermelho));
-            resposta.setText("MITO");
-        }*/
-        // -------------- // ---------------- //
-        //layout2.setVisibility(View.VISIBLE);
-       // layout1.setVisibility(View.GONE);
-        //btok.setVisibility(View.VISIBLE);*/
+           // resposta.setBackgroundColor(res.getColor(R.color.vermelho));
+           // resposta.setText("MITO");
+        }
+         -------------- // ---------------- //
+        layout2.setVisibility(View.VISIBLE);
+        layout1.setVisibility(View.GONE);
+        btok.setVisibility(View.VISIBLE);
     }
 
     public void botaoVerdade(View v){
-      /*  Resources res = getResources();
+        Resources res = getResources();
         String[] respostas = res.getStringArray(R.array.resposta);
         String[] explicacao = res.getStringArray(R.array.explicacao);
         txtexplicacao.setText(explicacao[contador]);
-        /*if (respostas[contador].equals("verdade")){
-            resposta.setBackgroundColor(res.getColor(R.color.verde));
-            resposta.setText("VERDADE");
+        if (respostas[contador].equals("verdade")){
+           // resposta.setBackgroundColor(res.getColor(R.color.verde));
+           // resposta.setText("VERDADE");
             pontos++;
         }
         else {
-            resposta.setBackgroundColor(res.getColor(R.color.vermelho));
-            resposta.setText("VERDADE");
-        }*/
-       /* layout1.setVisibility(View.VISIBLE);
+            //resposta.setBackgroundColor(res.getColor(R.color.vermelho));
+            //resposta.setText("VERDADE");
+        }
+        layout1.setVisibility(View.VISIBLE);
         layout2.setVisibility(View.GONE);
         btok.setVisibility(View.VISIBLE);
-        */
+
     }
 
     public void botaoOk(View v){
-     /*   Resources res = getResources();
+        Resources res = getResources();
         String[] perguntas = res.getStringArray(R.array.pergunta);
         if (contador == perguntas.length - 1){
             Intent i = new Intent(this, Fim.class);
@@ -95,7 +104,7 @@ public class Terapia extends AppCompatActivity {
             contador++;
             txtpergunta.setText(perguntas[contador]);
             layout1.setVisibility(View.GONE);
-            layout2.setVisibility(View.VISIBLE);*/
-        //}
+            layout2.setVisibility(View.VISIBLE);
+        }*/
     }
 }
